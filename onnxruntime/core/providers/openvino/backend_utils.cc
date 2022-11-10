2ae62dd0dc41ea28658129c62f234bfa969ae397
Preetha Veeramalai <preetha.veeramalai@intel.com>
2022-11-10 20:05:41 +0530

    Dump .xml, .bin and .dot files when debug is enabled

--- onnxruntime/core/providers/openvino/backend_utils.cc
+++ onnxruntime/core/providers/openvino/backend_utils.cc
@@ -207,6 +207,14 @@ CreateOVModel(const ONNX_NAMESPACE::ModelProto& model_proto, const GlobalContext
       --index;
     }
   }
+  #ifndef NDEBUG
+  if (IsDebugEnabled()) {
+    std::string name = cnn_network->get_friendly_name();
+    ov::pass::Serialize serializer(name + ".xml", name + ".bin");
+    serializer.run_on_model(cnn_network);
+    ngraph::plot_graph(cnn_network, name+"_executable" + ".dot");
+  }
+  #endif
   return cnn_network;
 }
 #endif
