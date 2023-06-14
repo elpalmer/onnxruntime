9c4384aa0b9c354bb6e171fa45a751527c70b13a
saurabhintel0 <saurabh1.kale@intel.com>
2023-06-14 17:22:21 +0530

    add ov libs to whl

--- setup.py
+++ setup.py
@@ -388,7 +388,7 @@ if is_manylinux:
             "libopenvino_intel_gpu_plugin.so",
             "libopenvino_auto_plugin.so",
             "libopenvino_hetero_plugin.so",
-            "libtbb.so.12"
+            "libtbb.so.12",
             "libtbb.so.12.2",
             "libtbbmalloc.so.2",
             "libopenvino.so",
